84849d34de5e329045255c29e047df8a07176f87
thiago <thiagofreitascarneiro>
2023-04-19 17:14:43 -0300

    creating handle to add cart

--- src/contexts/CyclesContexts.tsx
+++ src/contexts/CyclesContexts.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useContext, useReducer } from "react";
 import { State, globalReducer, initialState } from "../reducers/reducer";
+import { addToCart } from "../reducers/actions";
 
 interface ContextProps {
     state: State;
@@ -10,18 +11,30 @@ interface CyclesContextProviderProps {
   children: ReactNode;
 }
 
+export interface Action {
+  type: string;
+  payload?: any;
+}
+
+export interface AppContextType {
+  state: State;
+  dispatch: React.Dispatch<Action>;
+  handleAddToCart: (value: any) => void;
+}
+
 export const AppContext = createContext<ContextProps>({
   state: initialState,
-  dispatch: () => [],
+  dispatch: () => null,
 });
 
 export function AppProvider({ children}: CyclesContextProviderProps) {
   const [state, dispatch] = useReducer(globalReducer, initialState);
-  
 
-  
+  function handleAddToCart(value: any) {
+    dispatch(addToCart(value));
+  }  
   return (
-    <AppContext.Provider value={{ state, dispatch }}>
+    <AppContext.Provider value={{ state, dispatch, handleAddToCart }}>
       {children}
     </AppContext.Provider>
   );
